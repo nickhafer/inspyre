c7819d57bffc9fa79cfed2efbff24f05be16934c
Andrew Franks <andrewtfranks10@gmail.com>
2022-11-18 15:39:04 -0800

    imports

---
+++ App.js
@@ -0,0 +1,4 @@
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
