63f9410770e33da7fab48b8192337939e3ec342f
Andrew Franks <andrewtfranks10@gmail.com>
2022-11-18 15:55:15 -0800

    Starter Code
    Andrew Franks Edits with starter code

--- App.js
+++ App.js
@@ -1,4 +1,42 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AppLoading from 'expo-app-loading';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts } from 'expo-font';
+import { Themes } from './assets/Themes';
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Sydney: require('./assets/Fonts/Sydney-Serial-Regular.ttf'),
+    SydneyBold: require('./assets/Fonts/Sydney-Serial-Bold.ttf'),
+  });
+  if (!fontsLoaded) return <AppLoading />;
+  /* ^Don't mind/edit the code above, it's there to load the font for you! */
+  StatusBar.setBarStyle(Themes.light.statusBar);
+  /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly*/
+
+  /* insert your code here */
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
+        }}>
+        This is the Insypre app. Check Plus
+      </Text>
+      <Text
+        style={{
+          fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
+        }}>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
