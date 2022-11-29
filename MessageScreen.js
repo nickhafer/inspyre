8538b2f90161c87c83fe948e8c82060f9a243e23
Andrew Franks <andrewtfranks10@gmail.com>
2022-11-28 21:40:59 -0800

    Update

--- MessageScreen.js
+++ MessageScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, FlatList, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -8,43 +8,66 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function MessageScreen() {
 
-  const messageListData = [
+  const DATA = [
     {
       id: '1',
-      name: 'Joe',
-      //profile picture
+      name: 'Lucas',
+    },
+    {
+      id: '2',
+      name: 'Nichole',
     },
     {
       id: '3',
-      name: 'Momma',
-      //profile picture
+      name: 'Malcolm',
     },
-
-    {
-      id: '2',
-      name: 'Lisa',
-      //profile picture
-    },
+  ];
 
 
 
-
-  ]
-    return (
-      <View style={styles.screenContainer}>
-        <Text style={styles.screenText}>Message!</Text>
-      </View>
+  const Item = ({ name }) => (
+    <View style={styles.item}>
+      <Text style={styles.name}>{name}</Text>
+    </View>
+  );
+    
+    const renderItem = ({ item }) => (
+      <Item name={item.name} />
     );
-  };
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={item => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
 
 
-const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  screenText: {
-    fontSize: 32,
-  },
+  const styles = StyleSheet.create({
+    screenContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    screenText: {
+      fontSize: 32,
+    },
+    container: {
+      flex: 1,
+      marginTop: StatusBar.currentHeight || 0,
+    },
+
+    item: {
+      backgroundColor: '#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    name: {
+      fontSize: 32,
+    },
 });
