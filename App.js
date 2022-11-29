9e64b95ef523848523282e837e4dbad0d0830d80
nickhafer <107949449+nickhafer@users.noreply.github.com>
2022-11-28 21:08:25 -0800

    View -> SafeAreaView

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -6,33 +6,33 @@ import MessageScreen from './MessageScreen';
 
 function HomeScreen() {
   return (
-    <View style={styles.screenContainer}>
+    <SafeAreaView style={styles.screenContainer}>
       <Text style={styles.screenText}>Home!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 function CommunitiesScreen() {
   return (
-    <View style={styles.screenContainer}>
+    <SafeAreaView style={styles.screenContainer}>
       <Text style={styles.screenText}>Communities!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 function PostScreen() {
   return (
-    <View style={styles.screenContainer}>
+    <SafeAreaView style={styles.screenContainer}>
       <Text style={styles.screenText}>Post!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
 function ProfileScreen() {
   return (
-    <View style={styles.screenContainer}>
+    <SafeAreaView style={styles.screenContainer}>
       <Text style={styles.screenText}>Details!</Text>
-    </View>
+    </SafeAreaView>
   );
 }
 
