198f56d0cedb5cc92117fd75f969e381e7d3b46d
nickhafer <107949449+nickhafer@users.noreply.github.com>
2022-11-29 21:51:33 -0800

    static image added to feed

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, SafeAreaView, FlatList, StatusBar, TouchableHighlight, Pressable, Button, navigation } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, FlatList, StatusBar, Pressable, Image} from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -15,24 +15,27 @@ export default function HomeScreen() {
     const DATA = [
         {
             id: '1',
-            title: 'First Item',
-            item_name: 'broken chair',
+            title: 'Broken Chair',
             username: 'Nick',
-            picture: 'image1',
-            community: 'Stanford GSB',
-            distance: '1.2 miles',
-            rating: '5 stars',
-            num_likes: 32,
-            liked: false
+            image: 'image1',
+            // community: 'Stanford GSB',
+            // distance: '1.2 miles',
+            // rating: '5 stars',
+            // num_likes: 32,
+            // liked: false
         },
     ];
 
-    const Item = ({ title, username }) => (
+    const Item = ({ title, username, image }) => (
         <View style={styles.item}>
             <Text style={styles.title}>{title}</Text>
             <Text style={styles.username}>{username}</Text>
+            <Image
+                source={require('./assets/Images/image1.png')}
+                style={{ width: 200, height: 200 }}
+            />
             <Pressable 
-                onPress={() => navigation.navigate('Community')}
+                onPress={() => navigation.navigate('Chat')}
                 style={({ pressed }) => [
                     {
                         backgroundColor: pressed
@@ -53,6 +56,7 @@ export default function HomeScreen() {
         <Item
             title={item.title} 
             username={item.username}
+            image={item.image}
         />
     );
   
