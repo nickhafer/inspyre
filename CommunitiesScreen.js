870ba0660c45f6b164c0bb010fa5a73cb15db506
Andrew Franks <andrewtfranks10@gmail.com>
2022-12-03 16:04:02 -0800

    Communities

--- CommunitiesScreen.js
+++ CommunitiesScreen.js
@@ -1,4 +1,4 @@
-import { Image, Text, View, StyleSheet, SafeAreaView, FlatList, Pressable } from 'react-native';
+import { TextInput, Keyboard, Image, Text, View, StyleSheet, SafeAreaView, FlatList, Pressable } from 'react-native';
 import { useFonts } from 'expo-font';
 import CommunityScreen from './IndividualCommunityScreen';
 
@@ -92,6 +92,21 @@ export default function CommunitiesScreen ({ navigation }) {
                     <Image style={header_styles.headerText}
                     source={require("./FigmaIcons/messages.png")} />
                 </View>
+                <View style={header_styles.chatBottom}>
+                    <View style={header_styles.textInputView}>
+                        <TextInput
+                            style={header_styles.textInput}
+                            placeholder='Search'
+                            onSubmitEditing={Keyboard.dismiss}
+                            // onSubmitEditing={newText => setText(newText)}
+                            onChangeText={newText => setText(newText)}
+                        />
+                        {/* <Text style={styles.status}>{keyboardStatus}</Text> */}
+                    </View>
+                    <Image 
+                        style={header_styles.search} source={require('./assets/Icons/send-gray.png')} 
+                    />
+                </View>
                 <View style = {header_styles.communityTitle}>
                     <Text style = {header_styles.communityTitleText}>
                         Explore Communities
@@ -144,11 +159,128 @@ const header_styles = StyleSheet.create({
         //backgroundColor: 'blue',
         height: '5%',
         justifyContent: 'center',
+        marginTop: 10,
 
     },
     communityTitleText: {
         fontFamily: 'InterBold',
         fontSize: 30,
+
+    },
+    input: {
+        padding: 10,
+        borderWidth: 0.5,
+        borderRadius: 4
+    },
+    status: {
+        padding: 10,
+        textAlign: "center"
+    },
+    backgroundImage: {
+      width: '100%',
+      height: '100%'
+    },
+    body: {
+        backgroundColor: 'white',
+        height: '100%',
+        justifyContent: 'space-between',
+    },
+    chatSection: {
+        height: '70%',
+    },
+    topBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    back: {
+        width: 48,
+        height: 48,
+        marginLeft: 8,
+    },
+    messageHeader: {
+        width: '80%',
+        height: 80,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    profilePic: {
+        height: 64,
+        width: 64,
+        borderRadius: 32,
+    },
+    conversationWith: {
+        fontFamily: 'InterRegular',
+        fontSize: 16,
+        margin: 8,
+    },
+    name: {
+        fontFamily: 'InterBlack',
+        fontSize: 20,
+        margin: 8
+    },
+    messageTitle: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        //justifyContent: 'center',
+    },
+    messageDescription: {
+        fontSize: 14,
+        margin: 8,
+        fontFamily: 'InterRegular',
+        //backgroundColor: 'white',
+    },
+    messageDescriptionView: {
+        borderRadius: 8,
+        borderWidth: 2,
+        borderRadius: 16,
+        margin: 8,
+        paddingBottom: 10,
+        width: '50%',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    textInput: {
+        fontSize: 14,
+        marginTop: 16,
+        marginHorizontal: 8,
+    },
+    textInputView: {
+        alignSelf: 'flex-start',
+        borderRadius: 16,
+        borderColor: 'black',
+        borderWidth: 2,
+        alignItems: 'flex-start',
+        margin: 16,
+        paddingBottom: 10,
+        width: '75%',
+    },
+    // inputMessage: {
+    //     fontSize: 14,
+    //     marginTop: 16,
+    // },
+    inputMessageView: {
+        backgroundColor: 'lightgray',
+        borderWidth: 2,
+        borderRadius: 16,
+        alignSelf: 'flex-end',
+        borderRadius: 8,
+        alignItems: 'center',
+        margin: 8,
+        paddingBottom: 10,
+        width: '50%',
+    },
+    wholeScreen: {
+        backgroundColor: 'white',
+    },
+    chatBottom: {
+        flexDirection: 'row',
+        alignSelf: 'flex-start',
+        alignItems: 'center',
+    },
+    send: {
+        width: 40,
+        height: 40,
 
     },
 });
@@ -165,7 +297,8 @@ const styles = (item) => StyleSheet.create({
     },
     communityContainer: {
         //width: '30%',
-        height: '40%',
+        marginTop:5,
+        height: '30%',
         //flex: 5,
         //borderTopWidth: 1,
         //borderBottomWidth: 1,
@@ -199,6 +332,7 @@ const styles = (item) => StyleSheet.create({
         fontFamily: 'InterBold',
         alignSelf: 'center',
         backgroundColor: 'red',
+        //marginTop: 10,
     },
     exploreText: {
         fontSize: 16,
