dba5f644c4a735437077d6dc0734ec82a98a8c9f
nickhafer <107949449+nickhafer@users.noreply.github.com>
2022-12-05 21:44:29 -0800

    more pics

--- SelectPhotos.js
+++ SelectPhotos.js
@@ -9,16 +9,16 @@ export default function SelectPhotos ({ navigation }) {
             </View>
             <View style={styles.imageContainer}>
                 <Pressable onPress={() => navigation.navigate('PostDetails', 
-                    {image: 'https://www.goodshomedesign.com/wp-content/uploads/2021/11/old-new-chair-2.jpg'})}>
+                    {image: 'https://images.unsplash.com/photo-1616119547809-588a65fdc7e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'})}>
                     <Image 
-                        source={{ uri: 'https://www.goodshomedesign.com/wp-content/uploads/2021/11/old-new-chair-2.jpg'}}
+                        source={{ uri: 'https://images.unsplash.com/photo-1616119547809-588a65fdc7e8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80'}}
                         style={styles.objectImage}
                     />
                 </Pressable>
                 <Pressable onPress={() => navigation.navigate('PostDetails', 
-                    {image: 'https://www.goodshomedesign.com/wp-content/uploads/2021/11/old-new-chair-2.jpg'})}>
+                    {image: 'https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1296&q=80'})}>
                     <Image 
-                        source={{ uri: 'https://www.goodshomedesign.com/wp-content/uploads/2021/11/old-new-chair-2.jpg'}}
+                        source={{ uri: 'https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1296&q=80'}}
                         style={styles.objectImage}
                     />
                 </Pressable>
