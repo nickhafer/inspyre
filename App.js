c933d19b670c1bfd950f3c5d19b8fca6491f7f1f
nickhafer <107949449+nickhafer@users.noreply.github.com>
2022-12-07 17:31:08 -0800

    tab icons in

--- App.js
+++ App.js
@@ -25,15 +25,15 @@ export default function App() {
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused }) => {
             if (route.name === 'Home') {
-              return <Image source={focused ? require('./assets/Icons/chat-gray.png') : require('./assets/Icons/chat-white.png')}/>
+              return <Image source={focused ? require('./assets/Icons/home-green.png') : require('./assets/Icons/home-gray.png')}/>
             } else if (route.name === 'Profile') {
-              return <Image source={focused ? require('./assets/Icons/chat-gray.png') : require('./assets/Icons/chat-white.png')}/>
+              return <Image source={focused ? require('./assets/Icons/profile-green.png') : require('./assets/Icons/profile-gray.png')}/>
             } else if (route.name === 'Community') {
-              return <Image source={focused ? require('./assets/Icons/chat-gray.png') : require('./assets/Icons/chat-white.png')}/>
+              return <Image source={focused ? require('./assets/Icons/communities-green.png') : require('./assets/Icons/communities-gray.png')}/>
             } else if (route.name === 'Post') {
-              return <Image source={focused ? require('./assets/Icons/chat-gray.png') : require('./assets/Icons/chat-white.png')}/>
+              return <Image source={focused ? require('./assets/Icons/post-green.png') : require('./assets/Icons/post-gray.png')}/>
             } else if (route.name === 'Chat') {
-              return <Image source={focused ? require('./assets/Icons/chat-gray.png') : require('./assets/Icons/chat-white.png')}/>
+              return <Image source={focused ? require('./assets/Icons/chat-green.png') : require('./assets/Icons/chat-gray.png')}/>
             }
             return;
 
