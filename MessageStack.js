7ab21fc5ffdec7d1f58c7b8c145accaeb0602ea6
nickhafer <107949449+nickhafer@users.noreply.github.com>
2022-12-07 19:29:20 -0800

    individual items on dm feed

--- MessageStack.js
+++ MessageStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import PeopleScreen from './PeopleScreen';
 import DMScreen from './DMScreen';
 import HomeProfileScreen from './HomeProfileScreen';
+import IndividualItem from './IndividualItem';
 
 
 export default function MessageStack ({}) {
@@ -16,6 +17,7 @@ export default function MessageStack ({}) {
             <Stack.Screen name="PeopleScreen" component={PeopleScreen} />
             <Stack.Screen name="DMScreen" component={DMScreen} />
             <Stack.Screen name="HomeProfileScreen" component={HomeProfileScreen}/>
+            <Stack.Screen name="IndividualItem" component={IndividualItem}/>
         </Stack.Navigator>
     );
 };
